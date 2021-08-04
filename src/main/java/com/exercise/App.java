3855c6dd29760125041cd66be502cd65cb4220ed
Pranil Kharche <pranilkharche@gmail.com>
2021-08-04 12:35:52 +0530

    First Push

---
+++ src/main/java/com/exercise/App.java
@@ -0,0 +1,11 @@
+package com.exercise;
+
+import java.util.ArrayList;
+
+public class App {
+
+  public static void main(String args[]) {
+    ArrayList<String> list = new ArrayList<String>();
+    for (int i = 0; i < 100; i++) list.add(Integer.toString(i));
+  }
+}
